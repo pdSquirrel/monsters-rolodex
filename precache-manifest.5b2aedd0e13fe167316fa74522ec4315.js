b7ec7e848109b5743e3212534c7b128859f71c55
psSquirrel <progdev.squirrel@gmail.com>
2019-10-30 08:18:53 +0100

    Updates

---
+++ precache-manifest.5b2aedd0e13fe167316fa74522ec4315.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "525265bfafbff3e27835b17193824f18",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b51512bde76182e56cc2",
+    "url": "/monsters-rolodex/static/css/main.6beee427.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monsters-rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "b51512bde76182e56cc2",
+    "url": "/monsters-rolodex/static/js/main.71f77fde.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
